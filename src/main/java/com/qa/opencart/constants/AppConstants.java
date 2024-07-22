b36fc86421695ab7faa3a3a0abe7dc41139a3e74
LalithaSiri <Lalitha27523@gmail.com>
2024-07-22 12:30:29 -0700

    latest

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -9,7 +9,7 @@ public class AppConstants {
 	
 	public static final String LOGIN_PAGE_TITLE="Account Login";
 	public static final String LOGOUT_PAGE_TITLE="Account Logout";
-	public static final String ACCOUNTS_PAGE_TITLE="My Account!!";
+	public static final String ACCOUNTS_PAGE_TITLE="My Account";
 	public static final String PROPERTIESPATH = "C:\\HappyNewYear\\JavaWorkSpace\\OpenCartFullWebsite\\src\\main\\resources\\config\\config.properties";
 	public static final List<String> LISTACTUAL = Arrays.asList("My Account", "My Orders", "My Affiliate Account", "Newsletter");
 }
